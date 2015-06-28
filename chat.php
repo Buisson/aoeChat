808bf3f83c6cfefccab7113951b1bb2ee6a9ae5d
kyori-kyori <enj.clement@gmail.com>
2015-06-28 17:37:19 +0200

    Terminé !
    14

--- chat.php
+++ chat.php
@@ -1,26 +1,45 @@
 <?php
-    $nom = $_POST['nom'];                    //On récupère le pseudo et on le stocke dans une variable
-    $message = $_POST['message'];            //On fait de même avec le message   
+	$maxLenght = 200;
+	$DEBUG = isset($_GET['debug']);
     
-    if(empty($message) || empty($nom)) {
+    if($DEBUG)
+    {
+    	$_POST['nom'] = "test";                  
+    	$_POST['message'] = "haaa";
+    } 
+
+    if(isset($_GET['raz'])){
+    	$file = fopen('ac.htm',"w");
+    	ftruncate($file,0);
     	exit();
     }
 
-    $nom = htmlspecialchars($nom, ENT_QUOTES, 'UTF-8');
-    $message = htmlspecialchars($message, ENT_QUOTES, 'UTF-8');
+    if(!isset($_POST['nom']) || !isset($_POST['message'])) 
+    	exit();
+   
+    if(empty($_POST['nom']) || empty($_POST['message']))
+    	exit();
+    
 
+    $nom = htmlspecialchars($_POST['nom']);                    //On récupère le pseudo et on le stocke dans une variable
+    $message = htmlspecialchars($_POST['message']);            //On fait de même avec le message  
     $id = file('cpt.txt')[0] + 1;
     file_put_contents('cpt.txt', $id);
-
     $ligne = '<div>'.$nom.' > <span id="'.$id.'">'.$message.'</span></div>';     //Le message est créé 
 
+    if($DEBUG)
+    	echo 'Ligne : '.$ligne.'<hr>';
 
     $leFichier = file('ac.htm');             //On lit le fichier ac.htm et on stocke la réponse dans une variable (de type tableau)
 
+    if(count($leFichier) > $maxLenght) {
+    	array_shift($leFichier);
+    }
 
+    array_push($leFichier, $ligne."\n");       //On ajoute le texte calculé dans la ligne précédente au début du tableau
 
-    array_unshift($leFichier, $ligne);       //On ajoute le texte calculé dans la ligne précédente au début du tableau
+    if($DEBUG)
+   		print_r($leFichier);
 
-    $leFichier = array_reverse($leFichier);
     file_put_contents('ac.htm', $leFichier); //On écrit le contenu du tableau $leFichier dans le fichier ac.htm
 ?>
